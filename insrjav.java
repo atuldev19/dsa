07f9ba88f2e596314f19204b1ee395ee34f27cba
nsharma89 <71920486+nsharma89@users.noreply.github.com>
2020-10-02 13:55:16 +0530

    Update insrjav.java
    Optimized to O(N)

--- insrjav.java
+++ insrjav.java
@@ -1,42 +1,24 @@
-class InsertionSort { 
-	/*Function to sort array using insertion sort*/
-	void sort(int arr[]) 
-	{ 
-		int n = arr.length; 
-		for (int i = 1; i < n; ++i) { 
-			int key = arr[i]; 
-			int j = i - 1; 
-
-			/* Move elements of arr[0..i-1], that are 
-			greater than key, to one position ahead 
-			of their current position */
-			while (j >= 0 && arr[j] > key) { 
-				arr[j + 1] = arr[j]; 
-				j = j - 1; 
-			} 
-			arr[j + 1] = key; 
-		} 
-	} 
-
-	/* A utility function to print array of size n*/
-	static void printArray(int arr[]) 
-	{ 
-		int n = arr.length; 
-		for (int i = 0; i < n; ++i) 
-			System.out.print(arr[i] + " "); 
-
-		System.out.println(); 
-	} 
-
-	// Driver method 
-	public static void main(String args[]) 
-	{ 
-		int arr[] = { 12, 11, 13, 5, 6 }; 
-
-		InsertionSort ob = new InsertionSort(); 
-		ob.sort(arr); 
-
-		printArray(arr); 
-	} 
-} /* This code is contributed by Rajat Mishra. */
-
+import java.util.Arrays;
+public class InsertionSort {
+static void sort(int [] arrA){
+System.out.println("Original Array: " + Arrays.toString(arrA));
+for (int i = 0; i <arrA.length ; i++) {
+int j = i;
+while(j>0){
+//check if current element is smaller than previous element
+if(arrA[j]<arrA[j-1]){
+//swap the elements
+int temp = arrA[j];
+                    arrA[j] = arrA[j-1];
+                    arrA[j-1] = temp;
+                }
+                j--;
+            }
+        }
+System.out.println("(Insertion Sort)- Sorted Array: " +  Arrays.toString(arrA));
+    }
+public static void main(String[] args) {
+int [] arrA = {5, 1, 9, 3, 2, 10};
+        sort(arrA);
+    }
+}
