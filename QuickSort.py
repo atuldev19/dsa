150e20422161cba842bbab04f389a07d4e8afc76
unnati2201 <69833598+unnati2201@users.noreply.github.com>
2020-10-21 17:44:30 +0530

    Update QuickSort.py
    Quicksort using C++

--- QuickSort.py
+++ QuickSort.py
@@ -1,59 +1,73 @@
-# Python program for implementation of Quicksort Sort
-
-# This function takes last element as pivot, places
-# the pivot element at its correct position in sorted
-# array, and places all smaller (smaller than pivot)
-# to left of pivot and all greater elements to right
-# of pivot
-
-
-def partition(arr, low, high):
-	i = (low-1)		 # index of smaller element
-	pivot = arr[high]	 # pivot
-
-	for j in range(low, high):
-
-		# If current element is smaller than or
-		# equal to pivot
-		if arr[j] <= pivot:
-
-			# increment index of smaller element
-			i = i+1
-			arr[i], arr[j] = arr[j], arr[i]
-
-	arr[i+1], arr[high] = arr[high], arr[i+1]
-	return (i+1)
-
-# The main function that implements QuickSort
-# arr[] --> Array to be sorted,
-# low --> Starting index,
-# high --> Ending index
-
-# Function to do Quick sort
-
-
-def quickSort(arr, low, high):
-	if len(arr) == 1:
-		return arr
-	if low < high:
-
-		# pi is partitioning index, arr[p] is now
-		# at right place
-		pi = partition(arr, low, high)
-
-		# Separately sort elements before
-		# partition and after partition
-		quickSort(arr, low, pi-1)
-		quickSort(arr, pi+1, high)
-
-
-# Driver code to test above
-arr = [10, 7, 8, 9, 1, 5]
-n = len(arr)
-quickSort(arr, 0, n-1)
-print("Sorted array is:")
-for i in range(n):
-	print("%d" % arr[i]),
-
-# This code is contributed by Mohit Kumra
-#This code in improved by https://github.com/anushkrishnav
+/* C++ implementation of QuickSort */
+#include <bits/stdc++.h> 
+using namespace std;  
+  
+// A utility function to swap two elements  
+void swap(int* a, int* b)  
+{  
+    int t = *a;  
+    *a = *b;  
+    *b = t;  
+}  
+  
+/* This function takes last element as pivot, places  
+the pivot element at its correct position in sorted  
+array, and places all smaller (smaller than pivot)  
+to left of pivot and all greater elements to right  
+of pivot */
+int partition (int arr[], int low, int high)  
+{  
+    int pivot = arr[high]; // pivot  
+    int i = (low - 1); // Index of smaller element  
+  
+    for (int j = low; j <= high - 1; j++)  
+    {  
+        // If current element is smaller than the pivot  
+        if (arr[j] < pivot)  
+        {  
+            i++; // increment index of smaller element  
+            swap(&arr[i], &arr[j]);  
+        }  
+    }  
+    swap(&arr[i + 1], &arr[high]);  
+    return (i + 1);  
+}  
+  
+/* The main function that implements QuickSort  
+arr[] --> Array to be sorted,  
+low --> Starting index,  
+high --> Ending index */
+void quickSort(int arr[], int low, int high)  
+{  
+    if (low < high)  
+    {  
+        /* pi is partitioning index, arr[p] is now  
+        at right place */
+        int pi = partition(arr, low, high);  
+  
+        // Separately sort elements before  
+        // partition and after partition  
+        quickSort(arr, low, pi - 1);  
+        quickSort(arr, pi + 1, high);  
+    }  
+}  
+  
+/* Function to print an array */
+void printArray(int arr[], int size)  
+{  
+    int i;  
+    for (i = 0; i < size; i++)  
+        cout << arr[i] << " ";  
+    cout << endl;  
+}  
+  
+// Driver Code 
+int main()  
+{  
+    int arr[] = {10, 7, 8, 9, 1, 5};  
+    int n = sizeof(arr) / sizeof(arr[0]);  
+    quickSort(arr, 0, n - 1);  
+    cout << "Sorted array: \n";  
+    printArray(arr, n);  
+    return 0;  
+}  
