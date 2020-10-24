85cefc2c53297485a6d5464cfe91d7356b64d4ba
Ajitesh20 <44116591+ajitesh20@users.noreply.github.com>
2020-10-24 15:37:36 +0530

    Update HeapSort.cpp
    added user input and optimized code

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -1,10 +1,10 @@
 // C++ program for implementation of Heap Sort 
-#include <iostream> 
+#include <bits/stdc++.h> 
 using namespace std; 
 
 // To heapify a subtree rooted with node i which is 
 // an index in arr[]. n is size of heap 
-void heapify(int arr[], int n, int i) 
+void heapify(vector<int>& arr, int n, int i) 
 { 
 	int largest = i; // Initialize largest as root 
 	int l = 2 * i + 1; // left = 2*i + 1 
@@ -27,8 +27,8 @@ void heapify(int arr[], int n, int i)
 	} 
 } 
 
-// main function to do heap sort 
-void heapSort(int arr[], int n) 
+// main function to do heap sort
+void heapSort(vector<int> &arr, int n)
 { 
 	// Build heap (rearrange array) 
 	for (int i = n / 2 - 1; i >= 0; i--) 
@@ -44,22 +44,27 @@ void heapSort(int arr[], int n)
 	} 
 } 
 
-/* A utility function to print array of size n */
-void printArray(int arr[], int n) 
-{ 
-	for (int i = 0; i < n; ++i) 
-		cout << arr[i] << " "; 
-	cout << "\n"; 
-} 
-
 // Driver program 
 int main() 
 { 
-	int arr[] = { 12, 11, 13, 5, 6, 7 }; 
-	int n = sizeof(arr) / sizeof(arr[0]); 
+    int n;
+    cout<<"Enter the size of array\n";
+    cin>>n;
 
+	vector<int> arr(n);
+	for (auto &x : arr)
+	{
+        cin>>x;
+    }
+    
 	heapSort(arr, n); 
 
 	cout << "Sorted array is \n"; 
-	printArray(arr, n); 
+	
+    for(auto x: arr)
+    {
+        cout<<x<<" ";
+    }
+
+    cout<<'\n';
 } 
